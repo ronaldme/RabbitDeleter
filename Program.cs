2474d8cdca51da41c49cb020030781a331b9f7ac
ronaldm <ronaldme@gmail.com>
2017-02-08 14:10:53 +0100

    Add RabbitMQ deleter program
    - Configure host, username and password in app.config
    - Removes Queues
    - Removes Exchanges

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RabbitDeleter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var rabbitDeleter = new RabbitDeleter();
+                rabbitDeleter.RemoveAllQueues();
+                rabbitDeleter.RemoveAllExchanges();
+            }
+            catch (Exception e)
+            {
+                // some exchanges cannot be deleted
+            }
+
+            Console.WriteLine("Success!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitDeleter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using EasyNetQ.Management.Client;
+using EasyNetQ.Management.Client.Model;
+
+namespace RabbitDeleter
+{
+    public class RabbitDeleter
+    {
+        private readonly ManagementClient managementClient;
+
+        public RabbitDeleter()
+        {
+            managementClient = new ManagementClient(ConfigurationManager.AppSettings["host"],
+                ConfigurationManager.AppSettings["user"], ConfigurationManager.AppSettings["password"]);
+        }
+
+        public void RemoveAllQueues()
+        {
+            var queues = managementClient.GetQueues();
+
+            foreach (Queue queue in queues)
+            {
+                managementClient.DeleteQueue(queue);
+            }
+        }
+
+        public void RemoveAllExchanges()
+        {
+            var exchanges = managementClient.GetExchanges();
+
+            foreach (Exchange exchange in exchanges.Where(x => !string.IsNullOrEmpty(x.Name)))
+            {
+                try
+                {
+                    managementClient.DeleteExchange(exchange);
+                }
+                catch (Exception e)
+                {
+                    // some exchanges cannot be deleted
+                }
+            }
+        }
+    }
+}
